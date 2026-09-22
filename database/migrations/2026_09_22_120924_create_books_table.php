197ee7395f8ace0a84458a909527dc1a71aa92cd
Samuel Chingcuangco <chingcuangco.238863@munoz.sti.edu.ph>
2026-09-22 20:45:11 +0800

    made the books and authors schema and ran php artisan migrate

--- database/migrations/2026_09_22_120924_create_books_table.php
+++ database/migrations/2026_09_22_120924_create_books_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->foreignId('author_id')->constrained('authors')->cascadeOnDelete();
+            $table->date('published_date');
             $table->timestamps();
         });
     }
